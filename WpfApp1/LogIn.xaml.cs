30b47a125b27a0146b667cd7c6c63438fd1a2a89
Григорьев Дмитрий Викторович <10230906@college.local>
2025-11-24 10:28:07 +0300

    Добавил окно пользователя, добавил окно регистрации и окно входа

--- WpfApp1/LogIn.xaml.cs
+++ WpfApp1/LogIn.xaml.cs
@@ -26,58 +26,78 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Register register = new Register();
-            register.Show();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
             this.Close();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if (loginInput.Text.Length > 0)
+            Register register = new Register();
+            register.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (loginInput.Text.Length > 0 && passwordInput.Text.Length > 0)
             {
-                if (loginInput.Text == "admin")
+                if (loginInput.Text == "user" && passwordInput.Text == "12345")
                 {
-                    AdminPanel adminPanel = new AdminPanel();
-                    adminPanel.Show();
-                    this.Close();
-                }
-                else if (loginInput.Text == "manager")
-                {
-                    Manager manager = new Manager();
-                    manager.Show();
+                    User user = new User();
+                    user.Show();
                     this.Close();
                 }
             }
         }
 
-        private void loginInput_MouseEnter(object sender, MouseEventArgs e)
-        {
-            if (loginInput.Text == "Логин" && passwordInput.Text == "12345")
-            {
-                loginInput.Text = "";
-            }
-        }
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    if (loginInput.Text.Length > 0)
+        //    {
+        //        if (loginInput.Text == "admin")
+        //        {
+        //            AdminPanel adminPanel = new AdminPanel();
+        //            adminPanel.Show();
+        //            this.Close();
+        //        }
+        //        else if (loginInput.Text == "manager")
+        //        {
+        //            Manager manager = new Manager();
+        //            manager.Show();
+        //            this.Close();
+        //        }
+        //    }
+        //}
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                if (loginInput.Text.Length > 0)
-                {
-                    if (loginInput.Text == "admin")
-                    {
-                        AdminPanel adminPanel = new AdminPanel();
-                        adminPanel.Show();
-                        this.Close();
-                    }
-                    else if (loginInput.Text == "manager")
-                    {
-                        Manager manager = new Manager();
-                        manager.Show();
-                        this.Close();
-                    }
-                }
-            }
-        }
+        //private void loginInput_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    if (loginInput.Text == "Логин" && passwordInput.Text == "12345")
+        //    {
+        //        loginInput.Text = "";
+        //    }
+        //}
+
+        //private void Window_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Key == Key.Enter)
+        //    {
+        //        if (loginInput.Text.Length > 0)
+        //        {
+        //            if (loginInput.Text == "admin")
+        //            {
+        //                AdminPanel adminPanel = new AdminPanel();
+        //                adminPanel.Show();
+        //                this.Close();
+        //            }
+        //            else if (loginInput.Text == "manager")
+        //            {
+        //                Manager manager = new Manager();
+        //                manager.Show();
+        //                this.Close();
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
